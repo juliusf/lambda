1970ed4a00088bf64b2f8daf268a44c351d1204e
juliusf <julius.flohr@gmail.com>
2013-07-27 18:15:15 +0200

    converted to mono, converted GUI to use gtk-sharp. Added Mono.Texteditor

--- LambdaGUI/Program.cs
+++ LambdaGUI/Program.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
+using Gtk;
 
 namespace LambdaGUI
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
 }
