fb5e48b9cd3bc3060da37869cfd885ec6ec8d594
metafnord <bitbucket@metafnord.net>
2013-10-31 14:28:29 +0100

    fixed higher order function double evaluation bug....bitch

--- SchemeCore/SchemeEvaluator.cs
+++ SchemeCore/SchemeEvaluator.cs
@@ -4,6 +4,7 @@ using SchemeCore.objects;
 using SchemeCore;
 using SchemeCore.helper;
 using SchemeCore.builtin;
+using System.Diagnostics;
 
 
 
@@ -17,80 +18,80 @@ namespace SchemeCore
             root = SchemeEnvironmentRoot.instance;
 
             //instanciate the builtin Functions:
-            root.set( new SchemeSymbol( "+" ), new SchemeBuiltInPlus() );
-            root.set( new SchemeSymbol( "define" ), new SchemeBuitInDefine() );
-            root.set( new SchemeSymbol( "lambda" ), new SchemeBuiltInLambda() );
-            root.set( new SchemeSymbol( "=" ) , new SchemeBuiltinEquals() );
-            root.set( new SchemeSymbol( "if" ), new SchemeBuiltInIf() );
-            root.set( new SchemeSymbol( "modulo" ), new ScehemBuiltinModulo() );
-            root.set( new SchemeSymbol( "cons" ), new SchemeBuiltinCons() );
-            root.set( new SchemeSymbol( "cdr" ), new SchemeBuiltinCdr() );
-            root.set( new SchemeSymbol( "car" ), new SchemeBuiltinCar() );
+            root.set(new SchemeSymbol("+"), new SchemeBuiltInPlus());
+            root.set(new SchemeSymbol("define"), new SchemeBuitInDefine());
+            root.set(new SchemeSymbol("lambda"), new SchemeBuiltInLambda());
+            root.set(new SchemeSymbol("="), new SchemeBuiltinEquals());
+            root.set(new SchemeSymbol("if"), new SchemeBuiltInIf());
+            root.set(new SchemeSymbol("modulo"), new ScehemBuiltinModulo());
+            root.set(new SchemeSymbol("cons"), new SchemeBuiltinCons());
+            root.set(new SchemeSymbol("cdr"), new SchemeBuiltinCdr());
+            root.set(new SchemeSymbol("car"), new SchemeBuiltinCar());
 
         }
-        
-        public void loadSchemeLibrary( string path )
+
+        public void loadSchemeLibrary(string path)
         {
-            System.IO.StreamReader file = new System.IO.StreamReader( path );
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
             string libraryString = file.ReadToEnd();
             file.Close();
 
             var reader = new SchemeReader();
-            var ast = reader.parseString( libraryString );
+            var ast = reader.parseString(libraryString);
 
-            evaluate( ast );
+            evaluate(ast);
         }
 
-        public List<SchemeObject> evaluate( SchemeAST AST )
+        public List<SchemeObject> evaluate(SchemeAST AST)
         {
-            if( currentEnvironment == null )
+            if (currentEnvironment == null)
             {
                 currentEnvironment = root;
             }
-            return evaluate( ref AST);
+            return evaluate(ref AST);
         }
 
-        internal ISchemeEnvironment currentEnvironment  { get; set; } // this is necessary because the ref keyword does not allow polymorphism
-        internal  List<SchemeObject> evaluate( ref SchemeAST currentAST) 
+        internal ISchemeEnvironment currentEnvironment { get; set; } // this is necessary because the ref keyword does not allow polymorphism
+        internal List<SchemeObject> evaluate(ref SchemeAST currentAST)
         {
-           
+
             var returnValue = new List<SchemeObject>();
-            if( currentAST.currentObject == SchemeVoid.instance )
+            if (currentAST.currentObject == SchemeVoid.instance)
             {
-               for( int i = 0; i < currentAST.children.Count; i++ )
+                for (int i = 0; i < currentAST.children.Count; i++)
                 {
-                    var ast = (SchemeAST)currentAST.children[i] ;
-                    int instructionCount = 0; 
-                   while( true )
+                    var ast = (SchemeAST)currentAST.children[i];
+                    int instructionCount = 0;
+                    while (true)
                     {
 
-                       Logger.writeLine(String.Format("Instruction: {0}", instructionCount ++));
-                       if( Logger.enableConsoleLog || Logger.enableLogfile )
-                       {
-                           ast.createTreeOutput();
-                       }
-                           if( updateToNextLevelChild( ref ast, this.currentEnvironment ) ) //this updates currentAST until the first AST object is found which contains leaf objects only
+                        Logger.writeLine(String.Format("Instruction: {0}", instructionCount++));
+                        if (Logger.enableConsoleLog || Logger.enableLogfile)
+                        {
+                            ast.createTreeOutput();
+                        }
+                        if (updateToNextLevelChild(ref ast, this.currentEnvironment)) //this updates currentAST until the first AST object is found which contains leaf objects only
                         {
                             continue;
                         }
-                        var evaluated = evaluateSchemeAST( ref ast, this.currentEnvironment ); //evaluate the expression
+                        var evaluated = evaluateSchemeAST(ref ast, this.currentEnvironment); //evaluate the expression
 
-                        if( evaluated == null)
+                        if (evaluated == null)
                         {
-                           continue;
+                            continue;
                         }
 
 
-                        updateParent( ref ast, evaluated );                //replace currentAST with result
+                        updateParent(ref ast, evaluated);                //replace currentAST with result
 
-                        if( ast.parent.currentObject != SchemeVoid.instance )
+                        if (ast.parent.currentObject != SchemeVoid.instance)
                         {
-                                ast = ast.parent; //ascend the tree again
+                            ast = ast.parent; //ascend the tree again
                         }
                         else
                         {
-                          returnValue.Add( evaluated );
-                          break;
+                            returnValue.Add(evaluated);
+                            break;
                         }
                     }
                 }
@@ -99,41 +100,41 @@ namespace SchemeCore
         }
 
 
-        private bool updateToNextLevelChild( ref SchemeAST currentAst , ISchemeEnvironment environment)
+        private bool updateToNextLevelChild(ref SchemeAST currentAst, ISchemeEnvironment environment)
         {
             Type type = currentAst.currentObject.GetType();
 
-            if( type == typeof( SchemeSymbol ) )
+            if (type == typeof(SchemeSymbol))
             {
-                SchemeType t = environment.get( (SchemeSymbol) currentAst.currentObject );
-                if( t == null )
+                SchemeType t = environment.get((SchemeSymbol)currentAst.currentObject);
+                if (t == null)
                 {
-                    throw new SchemeUndefinedSymbolException( String.Format( "Undefined Symbol: {0}", currentAst.currentObject.ToString() ), currentAst.fileName, currentAst.sourceLength, currentAst.sourceOffset  );
+                    throw new SchemeUndefinedSymbolException(String.Format("Undefined Symbol: {0}", currentAst.currentObject.ToString()), currentAst.fileName, currentAst.sourceLength, currentAst.sourceOffset);
                 }
-                Type obj =t.GetType();
-                if( obj == typeof( SchemeBuiltInLambda ) ) // or typeof if. this is needed for parts which should not be evaluated.
+                Type obj = t.GetType();
+                if (obj == typeof(SchemeBuiltInLambda)) // or typeof if. this is needed for parts which should not be evaluated.
                 {
                     return false;
                 }
-                if( obj == typeof( SchemeBuiltInIf ) )
+                if (obj == typeof(SchemeBuiltInIf))
                 {
                     var tmpRoot = new SchemeAST();
                     var condition = currentAst.children[0];
                     var oldParent = condition.parent;
 
-                    tmpRoot.children.Add( condition );
+                    tmpRoot.children.Add(condition);
                     condition.parent = tmpRoot;
-                    var evaluated = evaluate( ref tmpRoot );    //evaluate the if condition
-                    tmpRoot = new SchemeAST(  currentAst, evaluated[0] );
+                    var evaluated = evaluate(ref tmpRoot);    //evaluate the if condition
+                    tmpRoot = new SchemeAST(currentAst, evaluated[0]);
                     currentAst.children[0] = tmpRoot;
 
                     return false;
                 }
             }
-            
-            foreach( SchemeAST child in currentAst.children )
+
+            foreach (SchemeAST child in currentAst.children)
             {
-                if( child.children.Count != 0 )
+                if (child.children.Count != 0)
                 {
                     currentAst = child;
                     return true;
@@ -142,99 +143,99 @@ namespace SchemeCore
             return false;
         }
 
-      
-        private void updateParent( ref SchemeAST currentAST, SchemeObject newValue )
+
+        private void updateParent(ref SchemeAST currentAST, SchemeObject newValue)
         {
-         /*   if (currentAST.currentObject.GetType() == typeof(SchemeLambda))
-            {
-                //  foreach (SchemeType obj in currrentEn )   
-                foreach (string key in currentEnvironment.getDict().Keys)
-                {
-                    currentEnvironment.parent().set(new SchemeSymbol(key), currentEnvironment.getDict()[key]);
-                }
-            } */
+            /*   if (currentAST.currentObject.GetType() == typeof(SchemeLambda))
+               {
+                   //  foreach (SchemeType obj in currrentEn )   
+                   foreach (string key in currentEnvironment.getDict().Keys)
+                   {
+                       currentEnvironment.parent().set(new SchemeSymbol(key), currentEnvironment.getDict()[key]);
+                   }
+               } */
             if (currentAST.hasOwnEnviornment)// && currentEnvironment.parent() != null )
             {
-                if ( newValue is SchemeLambda )
+                if (newValue is SchemeLambda)
                 {
                     foreach (string key in currentEnvironment.getDict().Keys)
                     {
                         currentEnvironment.parent().set(new SchemeSymbol(key), currentEnvironment.getDict()[key]);
-                    }  
+                    }
                 }
                 currentEnvironment = currentEnvironment.parent();
             }
 
-            int postition = currentAST.parent.children.IndexOf( currentAST );
-            currentAST.parent.children.Remove( currentAST );
-           // if(  newValue != SchemeVoid.instance )
-          //  {
-                var returnValue = new SchemeAST( currentAST.parent, newValue );
-                currentAST.parent.children.Insert( postition, returnValue );
-          //  }
+            int postition = currentAST.parent.children.IndexOf(currentAST);
+            currentAST.parent.children.Remove(currentAST);
+            // if(  newValue != SchemeVoid.instance )
+            //  {
+            var returnValue = new SchemeAST(currentAST.parent, newValue);
+            currentAST.parent.children.Insert(postition, returnValue);
+            //  }
 
-               
+
         }
 
-        private SchemeObject evaluateSchemeAST( ref SchemeAST ast, ISchemeEnvironment environment )
+        private SchemeObject evaluateSchemeAST(ref SchemeAST ast, ISchemeEnvironment environment)
         {
-           
-            
-            var func = getFunction( ref ast, environment );
-            if( func != null )
+
+
+            var func = getFunction(ref ast, environment);
+            if (func != null)
             {
-                return func.evaluate( ref ast, this );
+                return func.evaluate(ref ast, this);
             }
-            else  
+            else
             {
                 var type = getType(ref ast, environment);
-                if( type != null )
+                if (type != null)
                 {
-                     
+
                     return type;
                 }
                 else
-                {                      
-                    throw new SchemeNoSuchMethodException( String.Format( "{0} is no valid Scheme Method!", ast.currentObject.ToString() ) );
+                {
+                    throw new SchemeNoSuchMethodException(String.Format("{0} is no valid Scheme Method!", ast.currentObject.ToString()));
                 }
-            }   
-            
-        }
-
-        private ISchemeFunction getFunction( ref SchemeAST ast, ISchemeEnvironment environment )
-        {
-            SchemeObject ret = ast.currentObject;
-            if ( ast.currentObject.GetType() == typeof(SchemeSymbol) )
-            {
-                ret = environment.get( (SchemeSymbol) ret );
-            }  
-
-            if( !(  ret is ISchemeFunction)  )
-            {
-                return null; 
             }
-
-            return (ISchemeFunction) ret;
 
         }
 
-        private SchemeType getType( ref SchemeAST ast, ISchemeEnvironment environment )
+        private ISchemeFunction getFunction(ref SchemeAST ast, ISchemeEnvironment environment)
         {
             SchemeObject ret = ast.currentObject;
-            if( ast.currentObject.GetType() == typeof( SchemeSymbol ) )
+            if (ast.currentObject.GetType() == typeof(SchemeSymbol))
             {
-                ret = environment.get( (SchemeSymbol) ret );
+                ret = environment.get((SchemeSymbol)ret);
             }
 
-            if( !(ret is SchemeType ) )
+            if (!(ret is ISchemeFunction))
             {
                 return null;
             }
 
-            return (SchemeType) ret;
+            return (ISchemeFunction)ret;
+
         }
 
-       
+        private SchemeType getType(ref SchemeAST ast, ISchemeEnvironment environment)
+        {
+            SchemeObject ret = ast.currentObject;
+            if (ast.currentObject.GetType() == typeof(SchemeSymbol))
+            {
+                ret = environment.get((SchemeSymbol)ret);
+            }
+
+            if (!(ret is SchemeType))
+            {
+                return null;
+            }
+
+            return (SchemeType)ret;
+        }
+
+
     }
 }
 
--- SchemeCore/objects/SchemeLambda.cs
+++ SchemeCore/objects/SchemeLambda.cs
@@ -12,46 +12,46 @@ namespace SchemeCore.objects
         private SchemeAST _implementation;
         private ISchemeEnvironment _lambdaEnv;
 
-        public SchemeLambda( SchemeAST ast, ISchemeEnvironment currentEnv )
+        public SchemeLambda(SchemeAST ast, ISchemeEnvironment currentEnv)
         {
-            if( ast.children.Count < 2 )
-            { 
-                throw new SchemeWrongNumberOfArguments(String.Format("Lambda expects exactly two arguments. You have given me: {0}",ast.children.Count ));
+            if (ast.children.Count < 2)
+            {
+                throw new SchemeWrongNumberOfArguments(String.Format("Lambda expects exactly two arguments. You have given me: {0}", ast.children.Count));
             }
 
-            _lambdaEnv = new SchemeEnvironment(currentEnv); ;
+            _lambdaEnv = new SchemeEnvironment(currentEnv); 
             //really not beautiful, but this is the price we pay for using the AST data structure
-            if( ast.children[0].currentObject != SchemeVoid.instance ) // this is false for lambdas with no arguments
+            if (ast.children[0].currentObject != SchemeVoid.instance) // this is false for lambdas with no arguments
             {
-                _params.Add( (SchemeSymbol) ast.children[0].currentObject );
-                foreach( SchemeAST child in ast.children[0].children )
+                _params.Add((SchemeSymbol)ast.children[0].currentObject);
+                foreach (SchemeAST child in ast.children[0].children)
                 {
-                    _params.Add( (SchemeSymbol) child.currentObject );
+                    _params.Add((SchemeSymbol)child.currentObject);
                 }
             }
             _implementation = new SchemeAST();
 
-            
-
-                for( int i = 1; i < ast.children.Count; i++ )
-                {
-                    _implementation.children.Add( (SchemeAST) ast.children[i].Clone() );
-                    _implementation.children[_implementation.children.Count - 1].parent = ast;
-                }
 
 
-            }
-           
-        
-
-        public SchemeObject evaluate( ref SchemeAST currentAST, SchemeEvaluator evaluator )
-        {
-            if( currentAST.children.Count != _params.Count )
+            for (int i = 1; i < ast.children.Count; i++)
             {
-                throw new SchemeWrongNumberOfArguments( String.Format( "Lambda expects exactly two arguments. You have given me: {0}", currentAST.children.Count ) );
+                _implementation.children.Add((SchemeAST)ast.children[i].Clone());
+                _implementation.children[_implementation.children.Count - 1].parent = ast;
             }
 
-          if (evaluator.currentEnvironment != _lambdaEnv)
+
+        }
+
+
+
+        public SchemeObject evaluate(ref SchemeAST currentAST, SchemeEvaluator evaluator)
+        {
+            if (currentAST.children.Count != _params.Count)
+            {
+                throw new SchemeWrongNumberOfArguments(String.Format("Lambda expects exactly two arguments. You have given me: {0}", currentAST.children.Count));
+            }
+
+     /*       if (evaluator.currentEnvironment != _lambdaEnv)
             {
 
                 _lambdaEnv.setParent(evaluator.currentEnvironment);
@@ -61,19 +61,21 @@ namespace SchemeCore.objects
             else
             {
                 int i = 0;
-            }  
+            } */
+
+            evaluator.currentEnvironment = new SchemeEnvironment(evaluator.currentEnvironment);
 
 
-            for( int i = 0; i < _params.Count; i++ )
+            for (int i = 0; i < _params.Count; i++)
             {
                 var child = currentAST.children[i];
-                if( child.currentObject.GetType() == typeof( SchemeSymbol ) )
+                if (child.currentObject.GetType() == typeof(SchemeSymbol))
                 {
-                    var val = evaluator.currentEnvironment.parent().get( (SchemeSymbol) child.currentObject );
+                    var val = evaluator.currentEnvironment.parent().get((SchemeSymbol)child.currentObject);
 
-                    if( val == null )
+                    if (val == null)
                     {
-                       
+
                         val = evaluator.currentEnvironment.get((SchemeSymbol)child.currentObject);
 
                         if (val == null)
@@ -81,42 +83,42 @@ namespace SchemeCore.objects
                             throw new SchemeUndefinedSymbolException(String.Format("Undefined Symbol!: {0}", child.currentObject));
                         }
                     }
-                    evaluator.currentEnvironment.set( _params[i], val );
+                    evaluator.currentEnvironment.set(_params[i], val);
                 }
                 else
                 {
-                    evaluator.currentEnvironment.set( _params[i], (SchemeType) child.currentObject );
+                    evaluator.currentEnvironment.set(_params[i], (SchemeType)child.currentObject);
                 }
-               
+
             }
 
             var clonedImplementation = (SchemeAST)_implementation.Clone();
-            
+
             var oldParent = currentAST.parent;
-            var index = currentAST.parent.children.IndexOf( currentAST );
+            var index = currentAST.parent.children.IndexOf(currentAST);
             currentAST.parent.children.Remove(currentAST);
 
 
-            for( int i = 0; i < clonedImplementation.children.Count; i++ )
+            for (int i = 0; i < clonedImplementation.children.Count; i++)
             {
-                currentAST.parent.children.Insert( i + index, clonedImplementation.children[i] );
+                currentAST.parent.children.Insert(i + index, clonedImplementation.children[i]);
                 clonedImplementation.children[i].parent = oldParent;
 
-                if( i == clonedImplementation.children.Count - 1 )   // the last one being added
+                if (i == clonedImplementation.children.Count - 1)   // the last one being added
                 {
-                  currentAST.parent.children[i + index].hasOwnEnviornment = true; // set flag for enviornment switch
+                    currentAST.parent.children[i + index].hasOwnEnviornment = true; // set flag for enviornment switch
                 }
             }
 
 
             currentAST = currentAST.parent.children[index];
-            
-            return null ; //so ugly, but null means: to be evaluated again!
+
+            return null; //so ugly, but null means: to be evaluated again!
         }
 
         public override string ToString()
         {
-            return "Lambda"; 
+            return "Lambda";
         }
     }
 }
